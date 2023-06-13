b65b08a750b324a6cd9edb96f6c0e307a0ceb6a0
Romain <romain.poirrier@etudiant.univ-reims.fr>
2023-06-13 20:02:49 +0200

    Ajout de la méthode findBygenreId

--- src/Entity/Collection/FilmCollection.php
+++ src/Entity/Collection/FilmCollection.php
@@ -25,4 +25,22 @@ class FilmCollection
 
             return $request->fetchAll(PDO::FETCH_CLASS, Film::class);
     }
+
+    /**
+    * @param int $id
+    * @return array
+     */
+    public function findByGenreId(int $id): array {
+        $request = MyPdo::getInstance()->prepare(
+            <<<SQL
+                SELECT *
+                FROM film f
+                JOIN movie_genre m ON m.movieId = f.id
+                WHERE m.genreId = :genreId
+            SQL);
+
+        $request->execute([':genreId' => $id]);
+
+        return $request->fetchAll(PDO::FETCH_CLASS, Film::class);
+    }
 }
