7507e976f04793721e25c0cbe8cc262268b447e5
William PEOC'H <william.peoch@etudiant.univ-reims.fr>
2023-06-13 17:55:54 +0200

    Ajout de la classe People

--- src/Entity/People.php
+++ src/Entity/People.php
@@ -9,9 +9,10 @@ use PDO;
 
 class People
 {
+    private int $avatarId;
     private int $id;
     private string $birthday;
-    private string $deathday;
+    private ?string $deathday;
     private string $name;
     private string $biography;
     private string $placeOfBirth;
@@ -68,7 +69,7 @@ class People
     {
         $stmt = MyPDO::getInstance()->prepare(
             <<<'SQL'
-            SELECT *
+            SELECT id, avatarId, DATE_FORMAT(birthday, '%d/%m/%Y') AS birthday, DATE_FORMAT(deathday, '%d/%m/%Y') AS deathday, name, biography, placeOfBirth
             FROM people
             WHERE id = :id
             ORDER BY name
@@ -78,10 +79,10 @@ class People
         $stmt->execute([':id' => $id]);
 
         $stmt->setFetchMode(PDO::FETCH_CLASS, People::class);
-        if ($reqFetch = $stmt->fetch()) {
-            return $reqFetch;
+        if (($object = $stmt->fetch()) !== false) {
+            return $object;
         } else {
-            throw new EntityNotFoundException('Personne introuvable');
+            throw new EntityNotFoundException('Artiste introuvable');
         }
     }
 }
