06d42ad3aedf049a5b865778b21b226486483281
Romain <romain.poirrier@etudiant.univ-reims.fr>
2023-06-13 20:25:48 +0200

    Ajout de la méthode findById et des accesseurs

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -10,4 +10,37 @@ class Image
     private int $id;
     private string $jpeg;
 
+    /**
+     * @return int
+     */
+    public  function getId(): int{
+        return $this->id;
+    }/**
+     * @return string
+     */
+    public  function getJpeg(): string{
+        return $this->jpeg;
+    }
+
+    /**
+    * @param int $id
+    * @return Image
+     */
+    public function findbyId(int $id) : Image {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<SQL
+            SELECT *
+            FROM image
+            WHERE $id = :id;
+            SQL
+        );
+
+        $stmt -> execute([":id" => $id]);
+        $stmt -> setFetchMode(mode:PDO::FETCH_CLASS, className: Image::class);
+        if (($request = $stmt->fetch()) !== false) {
+            return $request;
+        } else {
+            throw new EntityNotFoundException('Image introuvable');
+        }
+    }
 }
