abc602abfccff583c67c62d3595575ea51e9de15
William PEOC'H <william.peoch@etudiant.univ-reims.fr>
2023-06-13 18:53:06 +0200

    Ajout des attributs de la classe Cast

---
+++ src/Entity/Cast.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class Cast
+{
+    private int $id;
+    private int $movieId;
+    private int $peopleid;
+    private string $role;
+    private int $orderIndex;
+}
