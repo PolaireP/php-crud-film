519a18c5dbb9ef80c0eb369d70fdbf8c24babaf0
William PEOC'H <william.peoch@etudiant.univ-reims.fr>
2023-06-13 19:13:27 +0200

    Ajout des accesseurs de la classe Cast

--- src/Entity/Cast.php
+++ src/Entity/Cast.php
@@ -4,11 +4,55 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+
 class Cast
 {
-    private int $id;
+    private ?int $id;
     private int $movieId;
-    private int $peopleid;
+    private int $peopleId;
     private string $role;
     private int $orderIndex;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMovieId(): int
+    {
+        return $this->movieId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPeopleId(): int
+    {
+        return $this->peopleId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrderIndex(): int
+    {
+        return $this->orderIndex;
+    }
+
+
 }
