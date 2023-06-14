2dc04fe99387d4615d198586691199023a72f6ab
William PEOC'H <william.peoch@etudiant.univ-reims.fr>
2023-06-14 12:48:13 +0200

    Ajout de la fiche personnelle des détails de l'acteur

--- public/acteur.php
+++ public/acteur.php
@@ -18,7 +18,49 @@ try {
     $webpage = new AppWebPage();
     $webpage->setTitle($webpage->escapeString("Films - {$people->getName()}"));
 
+    if ($people->getAvatarId() == null) {
+        $urlImg = "";
+    } else {
+        $urlImg = "image.php?imageId={$people->getAvatarId()}";
+    }
 
+    $webpage->appendContent(
+        <<<HTML
+        <div class="details_acteur">
+            <div class="vignette_acteur">
+                <img src="$urlImg">
+            </div>
+            <div class="infos_acteur">
+                <div class="nom">{$people->getName()}</div>
+        HTML
+    );
+
+    // Si le lieu de naissance n'est pas nulle on l'affiche
+    if ($people->getPlaceOfBirth() !== null) {
+        $webpage->appendContent("<div class='lieu_naissance'>{$people->getPlaceOfBirth()}</div>");
+    }
+
+    $webpage->appendContent("<div class='date'>");
+
+    // Si la date de naissance est renseignée l'affiché
+    if ($people->getBirthday() !== null) {
+        $webpage->appendContent("<div class='date_naissance'>{$people->getBirthday()}</div>");
+    }
+
+    // Si la date de décès est renseignée l'affiché
+    if ($people->getDeathday() !== null) {
+        $webpage->appendContent(" - <div class='date_mort'>{$people->getDeathday()}</div>");
+    }
+
+    // Fermeture de la div date
+    $webpage->appendContent("</div>");
+
+    // Si la biographie est renseignée l'affiché
+    if ($people->getBiography() !== null) {
+        $webpage->appendContent("<div class='biographie'>{$people->getBiography()}</div>");
+    }
+
+    $webpage->appendContent("</div></div>");
 
     echo $webpage->toHTML();
 
