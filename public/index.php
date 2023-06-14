42f22a84a8dd602652decf0b64b353ed320affee
Romain <romain.poirrier@etudiant.univ-reims.fr>
2023-06-14 12:09:33 +0200

    Ajout du générateur de page index.php

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+use Html\AppWebpage;
+use Entity\Collection\FilmCollection;
+use Entity\Image;
+
+$webpage = new AppWebpage('Films');
+
+$films = FilmCollection::findAll();
+
+foreach ($films as $movie) {
+    $webpage ->appendContent(
+        '<a class="movieCard"><img src="image.php?imageId='. $movie->getPosterId() .'"><h3 class="movieImage">'. $movie->getTitle() .'</h3></a>'. "\n");
+}
+
+$webpage->getLastModification();
+
+echo $webpage->toHTML();
+
