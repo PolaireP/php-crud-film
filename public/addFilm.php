4ca43168e8233d0c73fc14fa89aa13218222dc2b
Romain <romain.poirrier@etudiant.univ-reims.fr>
2023-06-14 18:57:07 +0200

    Création de addFilm - Fonctionnel

---
+++ public/addFilm.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+use Entity\Film;
+use Html\Form\FilmForm;
+use Entity\Collection\GenreCollection;
+use Database\MyPdo;
+
+$form = new FilmForm(null);
+$form -> setEntityFromQueryString();
+$form->getFilm()->save();
+
+$movieId = intval(MyPdo::getInstance()->lastInsertId());
+
+var_dump($_POST['genres']);
+$genres = $_POST['genres'];
+
+foreach ($genres as $key => $elem) {
+    GenreCollection::createMovieGenre($movieId, intval($elem));
+}
